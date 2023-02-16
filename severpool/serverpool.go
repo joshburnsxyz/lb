9c20d6db839dfd0958861c4ac89a75bcf1c8b7f5
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2023-02-16 23:36:23 +0000

    function to get the next peer backend in the list

--- severpool/serverpool.go
+++ severpool/serverpool.go
@@ -14,3 +14,18 @@ type ServerPool struct {
 func (s *ServerPool) NextIndex() int {
 	return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.backends)))
 }
+
+func (s *ServerPool) GetNextPeer() *backend.Backend {
+	var nexti = s.NextIndex()
+	l := len(s.backends) + nexti
+	for i := nexti; i < l; i++ {
+		idx := i % len(s.backends)
+		if s.backends[idx].IsAlive() {
+			if i != nexti {
+				atomic.StoreUint64(&s.current, uint64(idx))
+			}
+			return s.backends[idx]
+		}
+	}
+	return nil
+}
